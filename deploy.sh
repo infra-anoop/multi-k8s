ca5645522a04cc21e14f9fe4ae8eec46018339b7
Anoop Agarwal <anoop_agar_wal@yahoo.com>
2019-01-12 10:31:32 -0800

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t anoopagarwal/complex-client:latest -t anoopagarwal/complex-client:$SHA -f ./client/Dockerfile ./client
+docker build -t anoopagarwal/complex-server:latest -t anoopagarwal/complex-server:$SHA -f ./server/Dockerfile ./server
+docker build -t anoopagarwal/complex-worker:latest -t anoopagarwal/complex-worket:$SHA -f ./worker/Dockerfile ./worker
+
+docker push anoopagarwal/complex-client:latest
+docker push anoopagarwal/complex-server:latest
+docker push anoopagarwal/complex-worker:latest
+docker push anoopagarwal/complex-client:$SHA
+docker push anoopagarwal/complex-server:$SHA
+docker push anoopagarwal/complex-worker:$SHA
+
+kubectl apply -f k8s
+
+kubectl set image deployments/client-deployment client=anoopagarwal/complex-client:$SHA
+kubectl set image deployments/server-deployment server=anoopagarwal/complex-server:$SHA
+kubectl set image deployments/worker-deployment worker=anoopagarwal/complex-worker:$SHA
